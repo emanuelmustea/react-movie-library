707ad6ac156154650cb314b917bb78ee77d1155b
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2019-03-29 11:28:22 +0200

    implemented image carousel in movie page

--- src/MoviePageComponent/MoviePageComponent.js
+++ src/MoviePageComponent/MoviePageComponent.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import MoviesService from '../api/moviesService';
+import Carousel from '../carousel/Carousel';
 
 const movieSrc = imagePath => `https://image.tmdb.org/t/p/w500/${imagePath}`;
+
+const formatDate = (givenDate, onlyYear = true) => {
+  const d = new Date(givenDate);
+  const month = d.toLocaleString('en', { month: 'long' });
+  const day = d.getDate();
+  const year = d.getFullYear();
+  return onlyYear ? year : `${month} ${day}, ${year}`;
+};
 
 class MoviePage extends Component {
   constructor() {
@@ -18,21 +27,30 @@ class MoviePage extends Component {
   render() {
     if (this.state.hasError) return <div>we found an error</div>;
     if (!this.state.data) return <div>still loading post</div>;
-    console.log(this.props);
-    const { title, overview } = this.state.data;
     console.log(this.state.data);
+    const { title, overview, release_date, genre_ids, poster_path, backdrop_path } = this.state.data;
     return (
       <div className="row" style={{ padding: 30, paddingTop: 0 }}>
-        <div className="col five">
-          <div>
-            <img src={movieSrc(this.state.data.poster_path)} style={{ width: '100%' }} />
-          </div>
+        <div className="col three">
+          <Carousel
+            autoSlide={true}
+            slideAfter={3000}
+            transitionDuration={1000}
+            imgs={[movieSrc(poster_path), movieSrc(backdrop_path), movieSrc(poster_path)]}
+            auto
+          />
         </div>
         <div className="col grow">
           <div className="card">
             <div className="card-head">
               <button onClick={this.props.history.goBack}>⟨ Go back</button>
-              <h2>{title}</h2>
+              <h1>
+                <span>{title}</span>
+                <span className="grey lighter"> ({formatDate(release_date)})</span>
+              </h1>
+              <span>
+                {genre_ids.join(',')} | {formatDate(release_date, false)}
+              </span>
             </div>
             <div className="card-body">{overview}</div>
           </div>
